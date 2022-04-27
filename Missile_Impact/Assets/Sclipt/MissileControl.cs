cae8b9959bd1ac20bc8fd9ffec650cd941d03181
DESKTOP-G0BH34S\keigo <ogiek1016@gmail.com>
2022-04-28 00:37:51 +0900

    Move＋Asset

--- Missile_Impact/Assets/Sclipt/MissileControl.cs
+++ Missile_Impact/Assets/Sclipt/MissileControl.cs
@@ -1,6 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using System.Collections;
 
 public class MissileControl : MonoBehaviour
 {
@@ -28,7 +27,7 @@ public class MissileControl : MonoBehaviour
 		rigidbody = GetComponent<Rigidbody>();
 
 		// バネ復元力でゆらゆら揺れ続けるのを防ぐため、angularDragを大きめにしておく
-		rigidbody.angularDrag = 20.0f;
+		rigidbody.angularDrag = 15.0f;
 	}
 
 	void FixedUpdate()
@@ -57,37 +56,37 @@ public class MissileControl : MonoBehaviour
 	}
 }
 
-    /*
-    private float miuTurnInputValue;
-    private Rigidbody miuRb;
-    private float miuNoseInputValue;
+/*
+private float miuTurnInputValue;
+private Rigidbody miuRb;
+private float miuNoseInputValue;
 
-    public float missileSpeed;
+public float missileSpeed;
 
 
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        miuRb = GetComponent<Rigidbody>();
-    }
+// Start is called before the first frame update
+void Start()
+{
+	miuRb = GetComponent<Rigidbody>();
+}
 
-    // Update is called once per frame
-    void Update()
-    {
-        // 前進は自動
-        transform.Translate(0f, 0f , missileSpeed * Time.deltaTime);
+// Update is called once per frame
+void Update()
+{
+	// 前進は自動
+	transform.Translate(0f, 0f , missileSpeed * Time.deltaTime);
 
-        // 旋回
-        miuTurnInputValue = Input.GetAxis("Horizontal");
-        float turn = miuTurnInputValue * 150 * Time.deltaTime;
-        Quaternion turnRotation = Quaternion.Euler(0, turn, 0);
-        miuRb.MoveRotation(miuRb.rotation * turnRotation);
+	// 旋回
+	miuTurnInputValue = Input.GetAxis("Horizontal");
+	float turn = miuTurnInputValue * 150 * Time.deltaTime;
+	Quaternion turnRotation = Quaternion.Euler(0, turn, 0);
+	miuRb.MoveRotation(miuRb.rotation * turnRotation);
 
-        // 機首（上昇、下降）
-        miuNoseInputValue = Input.GetAxis("Vertical");
-        float noseTurn = -miuNoseInputValue * 60 * Time.deltaTime;
-        Quaternion turnNoseRotation = Quaternion.Euler(noseTurn, 0, 0);
-        miuRb.MoveRotation(miuRb.rotation * turnNoseRotation);
-    */
-    
+	// 機首（上昇、下降）
+	miuNoseInputValue = Input.GetAxis("Vertical");
+	float noseTurn = -miuNoseInputValue * 60 * Time.deltaTime;
+	Quaternion turnNoseRotation = Quaternion.Euler(noseTurn, 0, 0);
+	miuRb.MoveRotation(miuRb.rotation * turnNoseRotation);
+*/
+
